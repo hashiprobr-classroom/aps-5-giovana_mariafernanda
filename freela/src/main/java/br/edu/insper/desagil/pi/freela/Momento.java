8eaa1db0cead13a24da693edc8b718bfc424d501
Giovana <giovananzg@al.insper.edu.br>
2026-03-18 21:42:50 -0300

    Metodo

---
+++ freela/src/main/java/br/edu/insper/desagil/pi/freela/Momento.java
@@ -0,0 +1,16 @@
+public class Momento {
+
+    public Integer ajusta(int valor, int minimo, int maximo){
+        if(valor < minimo){
+            return minimo;
+        }else if(valor > maximo){
+            return maximo;
+        }else{
+            return valor;
+        }
+    }
+
+    public Integer minutos(){
+        return 1;
+    }
+}
---
+++ freela/src/main/java/br/edu/insper/desagil/pi/freela/Tarefa.java
@@ -0,0 +1,8 @@
+public class Tarefa {
+    private Integer id;
+    private String descricao;
+    private Integer inicio;
+    private Integer fim;
+
+
+}
