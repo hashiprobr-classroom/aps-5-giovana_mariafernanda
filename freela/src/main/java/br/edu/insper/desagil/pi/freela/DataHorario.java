96c9ab8840d5170a9930ba7759183d46538e3fc5
Giovana <giovananzg@al.insper.edu.br>
2026-03-20 10:36:06 -0300

    Começo da classe DataHorario

--- freela/src/main/java/br/edu/insper/desagil/pi/freela/DataHorario.java
+++ freela/src/main/java/br/edu/insper/desagil/pi/freela/DataHorario.java
@@ -1,2 +1,22 @@
-public class DataHorario {
+public class DataHorario extends Momento{
+    private Integer hora;
+    private Integer minuto;
+    private Data data;
+
+    public DataHorario(Integer hora, Integer minuto, Data data){
+        this.hora = 0;
+        this.minuto = 0;
+        this.data = data;
+    }
+
+    public Integer getHora(){
+        return hora;
+    }
+    public Integer getMinuto(){
+        return minuto;
+    }
+    public Data getData(){
+        return data;
+    }
+
 }
