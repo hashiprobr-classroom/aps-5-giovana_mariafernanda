db09fe4b1af0c07facd0331b82a1cd82828d117e
Giovana <giovananzg@al.insper.edu.br>
2026-03-20 18:30:46 -0300

    Classe Tarefa

--- freela/src/main/java/br/edu/insper/desagil/pi/freela/Tarefa.java
+++ freela/src/main/java/br/edu/insper/desagil/pi/freela/Tarefa.java
@@ -4,5 +4,19 @@ public class Tarefa {
     private Integer inicio;
     private Integer fim;
 
+    public Tarefa(Integer id, String descricao, Integer inicio, Integer fim){
+        this.id = id;
+        this.descricao = "";
+        this.inicio = null;
+        this.fim = null;
+    }
 
+    private void atualiza(Integer inicio, Integer fim){
+        if(inicio > fim){
+            throw new IllegalArgumentException("Erro: início deve ser antes do fim");
+        }
+
+        this.inicio = inicio;
+        this.fim = fim;
+    }
 }
